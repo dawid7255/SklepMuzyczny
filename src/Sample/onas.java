66c9b97c62a44368ff81c1aaca960f22efe64b94
dawid7255 <33196440+dawid7255@users.noreply.github.com>
2018-01-15 19:19:43 +0100

    MessageBox "O nas"

---
+++ src/Sample/onas.java
@@ -0,0 +1,45 @@
+package Sample;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.*;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class onas {
+    public static void messagebox(){
+        String title = "O mnie";
+        String message = "Projekt: Sklep muzyczny " +
+                "\n\nDawid Wiatrowski " +
+                "\n\nNumer albumu 129028" +
+                "\n\nInformatyka [WI]"+
+                "\n\nStudia niestacjonarne" +
+                "\n\nE-mail: " +
+                "\ndawid.wiatrowski@student.put.poznan.pl";
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(400);
+        window.setMinHeight(400);
+
+        javafx.scene.control.Label label = new javafx.scene.control.Label();
+        label.setText(message);
+        label.setTextAlignment(TextAlignment.CENTER);
+        label.setFont(new javafx.scene.text.Font("Times New Roman",20));
+
+
+        Button button = new Button("Zamknij");
+        button.setOnAction(event -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label,button);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.show();
+    }
+}
